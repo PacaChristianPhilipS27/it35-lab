dbe0fb9ddb14d4132d4bfa067acdb86eaef2169c
christianpaca <cybercion.xtian@gmail.com>
2025-02-07 08:11:57 +0800

    initail files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'it36-lab',
+  webDir: 'dist'
+};
+
+export default config;
