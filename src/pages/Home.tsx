66210b223fca1380612065785a4cdd91223cc8f5
PacaChristianPhilipS27 <cybercion.xtian@gmail.com>
2025-04-27 12:59:16 +0800

    fex the atbs foder place path and added feed container for the post

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,62 +10,53 @@ import {
       IonRouterOutlet, 
       IonTabBar, 
       IonTabButton, 
-      IonTabs,
-      IonTitle,
-
+      IonTabs, 
+      IonTitle, 
       IonToolbar 
-
-} from '@ionic/react';
-
+  } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { bookOutline, search, star } from 'ionicons/icons';
 import { Route, Redirect } from 'react-router';
 
-import Favorites from '../home-tabs/favorite';
-import Feed from '../home-tabs/feed';
-import Search from '../home-tabs/search';
-
-
-
+import Favorites from './home-tabs/favorite';
+import Feed from './home-tabs/feed';
+import Search from './home-tabs/search';
+  
   const Home: React.FC = () => {
 
-    const tabs = 
-  [
-    {name:'Feed', tab:'feed',url: '/it35-lab/app/home/feed', icon: bookOutline},
-    {name:'Search', tab:'search', url: '/it35-lab/app/home/search', icon: search},
-    {name:'Favorites',tab:'favorites', url: '/it35-lab/app/home/favorites', icon: star},
-
+    const tabs = [
+      {name:'Feed', tab:'feed',url: '/it35-lab/app/home/feed', icon: bookOutline},
+      {name:'Search', tab:'search', url: '/it35-lab/app/home/search', icon: search},
+      {name:'Favorites',tab:'favorites', url: '/it35-lab/app/home/favorites', icon: star},
+    ]
     
-  ]   
-
-return (
-
-  <IonReactRouter>
-    <IonTabs>
-
+    return (
+      <IonReactRouter>
+        <IonTabs>
           <IonTabBar slot="bottom">
+
             {tabs.map((item, index) => (
               <IonTabButton key={index} tab={item.tab} href={item.url}>
                 <IonIcon icon={item.icon} />
                 <IonLabel>{item.name}</IonLabel>
               </IonTabButton>
-
-))}
+            ))}
             
-</IonTabBar>
-<IonRouterOutlet>
+          </IonTabBar>
+        <IonRouterOutlet>
 
-<Route exact path="/it35-lab/app/home/feed" render={Feed} />
+          <Route exact path="/it35-lab/app/home/feed" render={Feed} />
           <Route exact path="/it35-lab/app/home/search" render={Search} />
           <Route exact path="/it35-lab/app/home/favorites" render={Favorites} />
+
           <Route exact path="/it35-lab/app/home">
             <Redirect to="/it35-lab/app/home/feed" />
           </Route>
 
-</IonRouterOutlet>
-     </IonTabs>
+        </IonRouterOutlet>
+        </IonTabs>
       </IonReactRouter>
-);
-};
-
- export default Home;
+    );
+  };
+  
+  export default Home;
