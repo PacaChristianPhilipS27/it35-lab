a2e4c2cb6069eda17832f2426f79fbf486fd7fcf
PacaChristianPhilipS27 <cybercion.xtian@gmail.com>
2025-04-27 11:34:40 +0800

    added textfield for login and text fiel dor password

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,35 +3,67 @@ import {
   IonButtons,
   IonContent,
   IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
   IonMenuButton,
   IonPage,
   IonTitle,
   IonToolbar,
-  useIonRouter
+  useIonRouter,
 } from '@ionic/react';
+import { useState } from 'react';
+
 function Login() {
   const navigation = useIonRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const doLogin = () => {
-    navigation.push('/it35-lab/app', 'forward', 'replace');
+    if (email.trim() !== '' && password.trim() !== '') {
+      // Here you can add real login logic
+      navigation.push('/it35-lab/app', 'forward', 'replace');
+    } else {
+      // Simple alert if fields are empty
+      alert('Please enter both email and password!');
+    }
   };
 
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className='ion-padding'>
-          <IonButton onClick={() => doLogin()} expand="full">
-            Login
-          </IonButton>
-        </IonContent>
-      </IonPage>
-    );
-  }
-  
-  export default Login;
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonItem>
+          <IonLabel position="floating">Email</IonLabel>
+          <IonInput
+            type="email"
+            value={email}
+            onIonChange={(e) => setEmail(e.detail.value!)}
+          />
+        </IonItem>
+
+        <IonItem>
+          <IonLabel position="floating">Password</IonLabel>
+          <IonInput
+            type="password"
+            value={password}
+            onIonChange={(e) => setPassword(e.detail.value!)}
+          />
+        </IonItem>
+
+        <IonButton onClick={doLogin} expand="full" className="ion-margin-top">
+          Login
+        </IonButton>
+      </IonContent>
+    </IonPage>
+  );
+}
+
+export default Login;
